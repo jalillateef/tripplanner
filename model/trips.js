c6a8dd119801154d35fb9b4ce5848ee012bdd5f2
jlateef <jlateef808@gmail.com>
2021-07-21 01:54:44 -0400

    updated files

---
+++ model/trips.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'Houston',
+        departure_date: '2021-07-31',
+        return_date: '2021-08-04'
+    },
+    {
+        id: 2,
+        title: 'Los Angeles',
+        departure_date: '2022-01-04',
+        return_date: '2022-01-10'
+    },
+    {
+        id: 3,
+        title: 'Miami',
+        departure_date: '2022-04-25',
+        return_date: '2022-05-03' 
+    }
+];
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require("http");
+const express = require('express');
+const trips = require("./model/trips");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const express = require('express');
-const app = express();
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the trip planner app');
+    res.render('home',{
+        title: "Trip Planner",
+        H1: "My Trips",
+        location: "Houston",
+        link: "New Trip"
+    });
 });
+
+app.get("/new", (req, res) => {
+    res.send("new",{
+
+    });
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
